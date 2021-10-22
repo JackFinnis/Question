2464b22aab45fd404099c90a8d325f723da6dda8
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-10-22 14:42:51 +0100

    Initial Commit

---
+++ Question/QuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp.swift
+//  Question
+//
+//  Created by Jack Finnis on 22/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
